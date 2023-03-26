030cc99b0cb887cf05c7258457ac7abaf3c2a885
배수한 <shbaeNhnacademy>
2023-03-26 22:13:19 +0900

    21608 정답

--- src/main/java/beakjoon/gold/g21608/Main.java
+++ src/main/java/beakjoon/gold/g21608/Main.java
@@ -57,64 +57,44 @@ public class Main {
                     }
 
                     if (loveCnt > max.loveCnt) {
-
-                    }
-
-
-                }
-            }
-
-            // 해당 학생이 좋아하는 학생이 주변에 최대 몇명 있는지 찾기
-            int loveMax = getLoveMax(N, loveArray, student);
-
-            // loveMax가 0이면 blank 찾기로 이동, 0이 아니면 loveMax에 맞는 값 찾기
-            if (loveMax == 0) {
-                // blank 찾기
-                findBlank(N, blankArray, student, new Stack<>());
-
-            } else {
-                // max에 맞는 값 찾아서 stack에 넣기
-                Stack<Point> pointStack = new Stack<>();
-                for (int i = 0; i < N; i++) {
-                    for (int j = 0; j < N; j++) {
-                        if (loveMax == loveArray[i][j]) {
-                            pointStack.add(new Point(i, j));
+                        max = new Info(blankCnt, loveCnt, j, i);
+                    } else if (loveCnt == max.loveCnt) {
+                        if (blankCnt > max.blank) {
+                            max = new Info(blankCnt, loveCnt, j, i);
+                        } else if (blankCnt == max.blank) {
+                            if (max.y > i) {
+                                max = new Info(blankCnt, loveCnt, j, i);
+                            } else if (max.y == i) {
+                                if (max.x > j) {
+                                    max = new Info(blankCnt, loveCnt, j, i);
+                                }
+                            }
                         }
                     }
                 }
-                if (pointStack.size() == 1) {
-                    Point point = pointStack.pop();
-                    answerArray[point.x][point.y] = student.myNum;
-                    continue;
-                } else {
-                    // blank 찾기
-                    findBlank(N, blankArray, student, pointStack);
-                }
             }
+            answerArray[max.y][max.x] = student.myNum;
         }
 
-        System.out.println(Arrays.deepToString(answerArray));
+//        System.out.println(Arrays.deepToString(answerArray));
 
 
 
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
                 int key = answerArray[i][j];
-                int[] loves = resultMap.get(key);
+                List<Integer> loves = resultMap.get(key);
 
                 // 상하좌우를 돌면서 주변에 좋아하는 학생의 갯수 카운트
                 int loveCnt = 0;
-                for (int next = 0; next < 4; next++) {
-                    for (int dir = 0; dir < 4; dir++) {
-                        int nx = j + dx[dir];
-                        int ny = i + dy[dir];
+                for (int dir = 0; dir < 4; dir++) {
+                    int nx = j + dx[dir];
+                    int ny = i + dy[dir];
 
-                        if (nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
+                    if (nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
 
-                        if (loves[next] == answerArray[ny][nx]) {
-                            loveCnt++;
-                            break;
-                        }
+                    if (loves.contains(answerArray[ny][nx])) {
+                        loveCnt++;
                     }
                 }
                 if (loveCnt == 1) {
@@ -130,122 +110,6 @@ public class Main {
             }
         }
         System.out.println(result);
-    }
-
-    static void findBlank(int N, int[][] blankArray, Student student, Stack<Point> pointStack) {
-        int blankMax = -1;
-
-        // 가장 처음 아무것도 없을때 동작
-        if (pointStack.isEmpty()) {
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < N; j++) {
-                    // 상하좌우를 돌면서 주변에 비어있는 칸 갯수 카운트
-                    for (int dir = 0; dir < 4; dir++) {
-                        int nx = j + dx[dir];
-                        int ny = i + dy[dir];
-
-                        if (nx < 0 || nx >= N || ny < 0 || ny >= N) {
-                            continue;
-                        }
-
-                        if (answerArray[i][j] != 0) {
-                            continue;
-                        }
-
-                        if (answerArray[ny][nx] == 0) {
-                            blankArray[i][j]++;
-                            blankMax = Math.max(blankArray[i][j], blankMax);
-                        }
-                    }
-                }
-            }
-
-            findSeat(N, blankArray, student, blankMax);
-        } else {
-            Point maxPoint = null;
-            while (!pointStack.isEmpty()) {
-                Point poll = pointStack.pop();
-
-                int i = poll.x;
-                int j = poll.y;
-                for (int dir = 0; dir < 4; dir++) {
-                    int nx = j + dx[dir];
-                    int ny = i + dy[dir];
-
-                    if (nx < 0 || nx >= N || ny < 0 || ny >= N) {
-                        continue;
-                    }
-
-                    if (answerArray[i][j] != 0) {
-                        continue;
-                    }
-
-                    if (answerArray[ny][nx] == 0) {
-                        blankArray[i][j]++;
-                        if (blankArray[i][j] >= blankMax) {
-                            blankMax = blankArray[i][j];
-                            maxPoint = poll;
-                        }
-                    }
-                }
-            }
-            if (maxPoint != null) {
-                answerArray[maxPoint.x][maxPoint.y] = student.myNum;
-            } else {
-                findSeat(N, blankArray, student, blankMax);
-            }
-            return;
-        }
-    }
-
-    static void findSeat(int N, int[][] blankArray, Student student, int blankMax) {
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if (blankMax == blankArray[i][j] || (blankMax == -1 && answerArray[i][j] == 0)) {
-                    answerArray[i][j] = student.myNum;
-                    return;
-                }
-            }
-        }
-    }
-
-    static int getLoveMax(int N, int[][] loveArray, Student student) {
-        int loveMax = 0;
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                // 정답 배열에 어떠한 값이 차있으면 확인할 필요 없음
-                if (answerArray[i][j] != 0) {
-                    continue;
-                }
-
-                // 상하좌우를 돌면서 주변에 좋아하는 학생의 갯수 카운트
-                int[] loves = student.loves;
-                for (int next = 0; next < 4; next++) {
-                    for (int dir = 0; dir < 4; dir++) {
-                        int nx = j + dx[dir];
-                        int ny = i + dy[dir];
-
-                        if (nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
-
-                        if (loves[next] == answerArray[ny][nx]) {
-                            loveArray[i][j]++;
-                            loveMax = Math.max(loveArray[i][j], loveMax);
-                            break;
-                        }
-                    }
-                }
-            }
-        }
-        return loveMax;
-    }
-
-    static class Point {
-        int x, y;
-
-        public Point(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
     }
 
     static class Info{
